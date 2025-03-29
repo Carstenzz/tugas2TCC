d821717f7808c1a71e550e686dab484a6e7d3471
Carstenzz <carstenzmp@gmail.com>
2025-03-29 23:18:19 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ import NotesRoute from "./routes/NotesRoutes.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false,
+}))
 app.use(express.json())
 app.use(NotesRoute)
 app.listen(4200, ()=>{console.log("alo bg ( ﾉ ﾟｰﾟ)ﾉ")})
